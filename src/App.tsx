a3d1ba16c138300ade007e293d812fdb012cdd3c
Vishesh Dadhich <visheshdadhich20@gmail.com>
2025-05-25 22:22:31 +0530

    included in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-// import Hamburger from "./Animations/Hamburger";
 import BottomNavbar from "./Animations/BottomNavbar";
+import Hamburger from "./Animations/Hamburger";
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      {/* <Hamburger /> */}
+    <div className="flex flex-col items-center justify-center h-screen gap-10">
+      <Hamburger />
       <BottomNavbar />
     </div>
   );
