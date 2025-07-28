fbbda560f8131c3b149953fe5b1eac313e319958
vishdadhich092004 <visheshdadhich20@gmail.com>
2025-07-28 19:50:03 +0530

    fix :[ routing ]

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
